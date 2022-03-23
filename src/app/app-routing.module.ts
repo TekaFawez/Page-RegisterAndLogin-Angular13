b4fa4bb5899548d1fd2d9f1ab25f2bda3343dad0
fawez <tekafawez@gmail.com>
2022-03-23 11:34:22 +0100

    seconde rep

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+
+    path:'',
+    component:RegisterComponent,
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+  },
+  {
+    
+      path:'register',
+      component:RegisterComponent,
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
